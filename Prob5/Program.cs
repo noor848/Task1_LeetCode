b4872b848fec7f79e13a057daef7196aba7f35c0
noor848 <54281955+noor848@users.noreply.github.com>
2022-07-16 22:42:20 +0300

    emptyLines & return statement in New Line

--- Prob5/Program.cs
+++ Prob5/Program.cs
@@ -1,8 +1,6 @@
-﻿public class Solution
-{
-    public int PivotIndex(int[] nums)
+﻿public class Program
+{ public int PivotIndex(int[] nums)
     {
-
         int left = 0, right = 0;
         int pivot = 0;
 
@@ -13,22 +11,16 @@
             int j = i;
             while (j < nums.Length)
             {
-
                 if (j + 1 < nums.Length)
                 {
                     right += nums[j + 1];
 
                 }
                 j++;
-
             }
-
-
             j = i;
             while (j > 0)
             {
-
-
                 if ((i - 1) >= 0)
                 {
                     left += nums[j - 1];
@@ -36,7 +28,8 @@
                 j--;
 
             }
-            if (left == right) return pivot;
+            if (left == right)
+                return pivot;
         }
 
         return -1;
